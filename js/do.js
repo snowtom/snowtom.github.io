8a5a5dcef224acfa04f039d9132eaa888a7a1d6e
snowtom <wumatjx7@gmail.com>
2015-08-26 19:27:19 +0800

    Update do.js

--- js/do.js
+++ js/do.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     $('.wrapper').on('mouseover', function () {
-        $(this).css('font-size', '0.6px');
+        $(this).css('font-size', '60px');
     });
     $('.wrapper').on('mouseout', function () {
-        $(this).css('font-size', '0.3px');
+        $(this).css('font-size', '40px');
     });
 });
